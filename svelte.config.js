97afe1ac04b457f7d5241bf3027aff15d8682376
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-23 09:55:04 -0800

    fix: added preprocessor for ts

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +13,9 @@ const config = {
       $utils: './src/utils',
     },
   },
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 };
 
 export default config;
