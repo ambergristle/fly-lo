ca9e670065a37bc11b47de0b8d3a575509b6e3b5
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-24 15:13:05 -0800

    feat: v1 form implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
+   import { Loader2 } from 'lucide-svelte';
+  import { enhance } from '$app/forms';
+  
+  import { Button } from '$lib/components/button';
+  import { DateRangePicker } from '$lib/components/date-range-picker';
   import * as Table from '$lib/components/table';
-  import type { PageData } from './$types';
+
+  import type { ActionData, PageData, SubmitFunction } from './$types';
 
   export let data: PageData;
-  console.log(data);
+  export let form: ActionData;
+
+  // console.log(data);
+  // console.log(form);
+
+  let loading = false;
+
+  const queryMonth: SubmitFunction = (options) => {
+    /** @todo validate */
+    loading = true;
+
+    return async (options) => {
+      loading = false;
+      /** must manually call default post-submit behavior */
+      await options.update();
+    };
+  };
+
 </script>
+
+<form method="POST" use:enhance={queryMonth}>
+  <DateRangePicker />
+  <Button type="submit" disabled={loading}>
+    {#if loading}
+      <Loader2 class="mr-2 h-4 w-4 animate-spin" />
+    {/if}
+    Search
+  </Button>
+</form>
 
 <Table.Root>
   <Table.Caption>
@@ -23,18 +56,5 @@
     </Table.Row>
   </Table.Header>
   <Table.Body>
-    {#each data.results as result}
-      <Table.Row>
-        <Table.Cell>
-          {result.airline}
-        </Table.Cell>
-        <Table.Cell>
-          {result.points}
-        </Table.Cell>
-        <Table.Cell>
-          {'date'}
-        </Table.Cell>
-      </Table.Row>
-    {/each}
   </Table.Body>
 </Table.Root>
