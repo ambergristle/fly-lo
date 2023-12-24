8dbff525c72d4beb34bc78d39870dd23e938f1b7
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-24 14:45:18 -0800

    chore: added loading icon + button components

---
+++ src/lib/components/loading-icon/loading-icon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let className: string;
+  export { className as class };
+</script>
+
+<div class="h-fit w-fit">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class={className}
+  >
+    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+  </svg>
+  <span class="sr-only">Loading...</span>
+</div>
