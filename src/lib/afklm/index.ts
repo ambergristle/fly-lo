3dac169b5ffe2e2b1699c91863f1a2aeea382ade
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-24 14:16:43 -0800

    feat: set up query via form actions

---
+++ src/lib/afklm/index.ts
@@ -0,0 +1,161 @@
+import wretch from 'wretch';
+import { z } from 'zod';
+
+enum Provider {
+  AirFrance = 'AF',
+  KLM = 'KLM',
+  TurkishAir = 'TKA',
+}
+
+type OfferRow = {
+  origin: {
+    name: string;
+    code: string;
+  };
+  destination: {
+    name: string;
+    code: string;
+  };
+  // split somehow?
+  departsAt: Date;
+  arrivesAt: Date;
+  duration: number;
+  airline: Provider;
+  flightId: string; // number?
+  price: {
+    miles: number;
+    taxes: number;
+  }
+}
+
+type LowestFareOffersResponse = z.infer<typeof LowestFareOffersResponseSchema>;
+const LowestFareOffersResponseSchema = z.object({
+  connections: z.object({
+    id: z.number(),
+    dateInterval: z.string(),
+    departureDate: z.string(),
+    origin: z.object({
+      code: z.string(),
+      name: z.string(),
+    }),
+    destination: z.object({
+      code: z.string(),
+      name: z.string(),
+    }),
+    duration: z.number(),
+  }).array().array(),
+  recommendations: z.object({
+    flightProducts: z.object({
+      priceInMiles: z.object({
+        displayPrice: z.number(),
+        totalPrice: z.number(),
+      }),
+      connections: z.object({
+        connectionId: z.number(),
+        commercialCabin: z.enum(['BUSINESS']),
+        numberOfSeatsAvailable: z.number(),
+        _links: z.object({
+          availableOffers: z.object({
+            href: z.string(),
+          }).optional(),
+        }),
+      }).array(),
+      taxDetails: z.object({
+        totalPrice: z.number(),
+      }), 
+    }).array(),
+  }).array(),
+});
+
+const AFKLM = wretch(process.ENV.AFKLM_BASE_URL)
+  .headers({
+    'accept-language': 'en-US',
+    'api-key': process.env.AFKLM_KEY,
+    'afkl-travel-host': 'AF',
+  });
+
+export const lowestFareOffers = async ({
+  origin,
+  destination,
+  filter,
+  format,
+}: {
+  origin: string;
+  destination: string;
+  filter: {
+    fromDate: string;
+    toDate: string;
+    interval: 'DAY' | 'MONTH';
+  },
+  format: {
+    currency: 'USD' | 'EUR';
+  }
+}) => {
+  const dateInterval = `${filter.fromDate}/${filter.toDate}`;
+
+  return await AFKLM
+    .url('/lowest-fare-offers')
+    .post({
+      'bookingFlow': 'REWARD',
+      'commercialCabins': ['BUSINESS'],
+      'customer': {},
+      'passengers': [{
+        'id': 0,
+        'type': 'ADT',
+      }, {
+        'id': 1,
+        'type': 'ADT',
+      }],
+      'requestedConnections': [{
+        'dateInterval': dateInterval,
+        'origin': {
+          'type': 'AIRPORT',
+          'code': origin,
+        },
+        'destination': {
+          'type': 'AIRPORT',
+          'code': destination,
+        },
+      }],
+      'type': filter.interval,
+      'currency': format.currency,
+    })
+    .json(LowestFareOffersResponseSchema.parse);
+};
+
+export const queryLowestFareOffers = async (params: {
+  origin: string;
+  destination: string;
+  filter: {
+    fromDate: string;
+    toDate: string;
+    interval: 'DAY' | 'MONTH';
+  },
+  format: {
+    currency: 'USD' | 'EUR';
+  }
+}) => {
+  const response = await lowestFareOffers(params);
+  const connections = response.connections[0];
+  
+  return response.recommendations
+    .map((recommendation, index) => {
+      const flightProduct = recommendation.flightProducts[0];
+      const c = flightProduct.connections[0];
+
+      const connection = connections[index];
+      if (connection.id !== c.connectionId) throw new Error('Failed to match connection: Invalid ID');
+
+      return {
+        origin: connection.origin.code,
+        destination: connection.origin.code,
+        commercialCabin: c.commercialCabin,
+        departureDate: connection.departureDate,
+        duration: connection.duration,
+        miles: flightProduct.priceInMiles.totalPrice,
+        taxes: flightProduct.taxDetails.totalPrice,
+        numberOfSeatsAvailable: c.numberOfSeatsAvailable,
+        offers: c._links.availableOffers?.href,
+      };
+    });
+};
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,45 @@
-import type { PageServerLoad } from './$types';
+// import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+import { queryLowestFareOffers } from '$lib/afklm';
 
 export const load: PageServerLoad = ({ url, route, params }) => {
   console.log({ url, route, params });
+
   return {
-    results: [
-      {
-        airline: 'AFKLM',
-        origin: 'SFO',
-        destination: 'ATH',
-        points: 50000,
-      },
-    ],
+  //   offers: lowestFareOffers({
+  //     origin: 'SFO',
+  //     destination: 'ATH',
+  //     filter: {
+  //       fromDate: '2024-01-01',
+  //       toDate: '2024-12-31',
+  //       interval: 'DAY',
+  //     },
+  //     format: {
+  //       currency: 'USD',
+  //     },
+  //   }),
   };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const result = await queryLowestFareOffers({
+      origin: 'SFO',
+      destination: 'ATH',
+      filter: {
+        fromDate: '2024-01-01',
+        toDate: '2024-12-31',
+        interval: 'DAY',
+      },
+      format: {
+        currency: 'USD',
+      },
+    });
+
+    return {
+      success: true,
+      data: result,
+    };
+  },
 };
