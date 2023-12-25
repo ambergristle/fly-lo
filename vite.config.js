601230e1d0170ff3f8a7cbb7eb713afad38f716d
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-25 08:59:02 -0800

    fix: hopefully this will run on node

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   server: { proxy: {} },
-  plugins: [basicSsl(), sveltekit()],
+  plugins: [sveltekit()],
 });
