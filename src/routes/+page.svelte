9dec8771097a71af16b1df26155f95d7069fd7b3
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-24 21:47:50 -0800

    style: wrapped form in card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,7 @@
 
   import { Button } from '$lib/components/button';
   import { Field, Form, FormButton, FormItem, Input } from '$lib/components/form';
+  import * as Card from '$lib/components/card';
   import * as Popover from '$lib/components/popover';
   import { RangeCalendar } from '$lib/components/range-calendar';
   import * as Table from '$lib/components/table';
@@ -47,78 +48,85 @@
   let datePickerStartValue: DateValue | undefined = undefined;
 </script>
 
-<Form
-  schema={QueryValues}
-  form={formProps}
-  controlled
-  let:config
-  class="flex flex-row items-center"
->
-  <Field {config} name="origin">
-    <FormItem>
-      <Input placeholder="SFO"/>
-    </FormItem>
-  </Field>
-  <Field {config} name="destination">
-    <FormItem>
-      <Input placeholder="ATH"/>
-    </FormItem>
-  </Field>
-  <Popover.Root openFocus>
-    <Popover.Trigger asChild let:builder>
-      <Button
-        variant="outline"
-        class={cn(
-          'w-[300px] justify-start text-left font-normal',
-          !datePickerValue && 'text-muted-foreground',
-        )}
-        builders={[builder]}
-      >
-        <CalendarIcon class="mr-2 h-4 w-4" />
-        {#if datePickerValue && datePickerValue.start}
-          {#if datePickerValue.end}
-            {df.format(datePickerValue.start.toDate(getLocalTimeZone()))} - {df.format(
-              datePickerValue.end.toDate(getLocalTimeZone()),
+<Card.Root>
+  <Card.Content class="grid gap-4">
+    <Form
+      schema={QueryValues}
+      form={formProps}
+      controlled
+      let:config
+      class="flex flex-row items-center"
+    >
+      <div class="text-lg font-semibold">
+        fly-lo
+      </div>
+      <Field {config} name="origin">
+        <FormItem>
+          <Input placeholder="SFO"/>
+        </FormItem>
+      </Field>
+      <Field {config} name="destination">
+        <FormItem>
+          <Input placeholder="ATH"/>
+        </FormItem>
+      </Field>
+      <Popover.Root openFocus>
+        <Popover.Trigger asChild let:builder>
+          <Button
+            variant="outline"
+            class={cn(
+              'w-[300px] justify-start text-left font-normal',
+              !datePickerValue && 'text-muted-foreground',
             )}
-          {:else}
-            {df.format(datePickerValue.start.toDate(getLocalTimeZone()))}
-          {/if}
-        {:else if datePickerStartValue}
-          {df.format(datePickerStartValue.toDate(getLocalTimeZone()))}
+            builders={[builder]}
+          >
+            <CalendarIcon class="mr-2 h-4 w-4" />
+            {#if datePickerValue && datePickerValue.start}
+              {#if datePickerValue.end}
+                {df.format(datePickerValue.start.toDate(getLocalTimeZone()))} - {df.format(
+                  datePickerValue.end.toDate(getLocalTimeZone()),
+                )}
+              {:else}
+                {df.format(datePickerValue.start.toDate(getLocalTimeZone()))}
+              {/if}
+            {:else if datePickerStartValue}
+              {df.format(datePickerStartValue.toDate(getLocalTimeZone()))}
+            {:else}
+              Pick a date
+            {/if}
+          </Button>
+        </Popover.Trigger>
+        <Popover.Content class="w-auto p-0" align="start">
+          <RangeCalendar
+            bind:value={datePickerValue}
+            bind:startValue={datePickerStartValue}
+            initialFocus
+            numberOfMonths={2}
+            placeholder={datePickerValue?.start}
+            minValue={today(getLocalTimeZone())}
+            onValueChange={(v) => {
+              $form.dateRange.start = v.start
+                ? v.start.toString()
+                : '';
+                
+              $form.dateRange.end = v.end
+                ? v.end.toString()
+                : '';
+            }}
+          />
+        </Popover.Content>
+      </Popover.Root>
+      <FormButton>
+        {#if $delayed}
+          <LoadingIcon class="mr-2 h-4 w-4 animate-spin" />
         {:else}
-          Pick a date
+          <SearchIcon class="mr-2 h-4 w-4" />
         {/if}
-      </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0" align="start">
-      <RangeCalendar
-        bind:value={datePickerValue}
-        bind:startValue={datePickerStartValue}
-        initialFocus
-        numberOfMonths={2}
-        placeholder={datePickerValue?.start}
-        minValue={today(getLocalTimeZone())}
-        onValueChange={(v) => {
-          $form.dateRange.start = v.start
-            ? v.start.toString()
-            : '';
-            
-          $form.dateRange.end = v.end
-            ? v.end.toString()
-            : '';
-        }}
-      />
-    </Popover.Content>
-  </Popover.Root>
-  <FormButton>
-    {#if $delayed}
-      <LoadingIcon class="mr-2 h-4 w-4 animate-spin" />
-    {:else}
-      <SearchIcon class="mr-2 h-4 w-4" />
-    {/if}
-    Search
-  </FormButton>
-</Form>
+        Search
+      </FormButton>
+    </Form>
+  </Card.Content>
+</Card.Root>
 
 <Table.Root>
   <Table.Caption>
