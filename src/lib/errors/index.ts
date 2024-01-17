556c8a40a4979ad6ac2eeaf515f432933eba20ab
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-01-17 08:16:38 -0800

    feat: added helper text and json getter to errors

--- src/lib/errors/index.ts
+++ src/lib/errors/index.ts
@@ -2,10 +2,17 @@
 class ServiceError extends Error {
   public readonly status: number;
 
-  constructor(status: number, message: string) {
+  public readonly message: string;
+  public readonly helperText: string;
+
+  constructor(status: number, message: string, helperText: string) {
     super(message);
 
+    this.name = 'ServiceError';
+    this.message = message;
+
     this.status = status;
+    this.helperText = helperText;
   }
 }
 
@@ -13,14 +20,19 @@ export class RateLimitError extends ServiceError {
   public readonly retryAfterSeconds: number | null;
 
   constructor(retryAfterSeconds: number) {
-    super(402, 'Rate limit exceeded');
+    const retryAfterMinutes = retryAfterSeconds / 60;
+    const retryAfterDisplay = retryAfterMinutes > 60
+      ? `${(retryAfterMinutes / 60).toFixed(1)} hours`
+      : `${retryAfterMinutes.toFixed(0)} minutes`;
+
+    super(402, 'Rate limit exceeded', `Please try again after ${retryAfterDisplay}`);
+
+    this.name = 'RateLimitError';
 
     this.retryAfterSeconds = retryAfterSeconds;
   }
 
-  get data() {
-    return {
-      retryAfterSeconds: this.retryAfterSeconds,
-    };
+  get json() {
+    return JSON.parse(JSON.stringify(this));
   }
 }
