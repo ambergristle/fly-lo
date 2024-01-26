90ae0ebc55e698f830680a389a4ad5f82cd82bdb
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-01-25 21:07:56 -0800

    refactor: switch to action/redirect load pattern

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,75 @@
 <script lang="ts">
+  import { superForm } from 'sveltekit-superforms/client';
+  import {
+    Loader2 as LoadingIcon, 
+    Search as SearchIcon, 
+  } from 'lucide-svelte';
 
-  import type { ActionData, PageData } from './$types';
+  import type { PageData } from './$types';
+
+  import * as Card from '$lib/components/card';
+  import * as Form from '$lib/components/form';
   import DataTable from './data-table.svelte';
-  import SearchBar from './search-bar.svelte';
-  
+  import { ZQueryValues } from './schemas';
+      
   export let data: PageData;
-  export let form: ActionData;
 
+  const form = superForm(data.form, {
+    dataType: 'json',
+  });
 </script>
 
 <div class="container">
-  <SearchBar query={data.query} />
-  <DataTable results={form?.results} />
+  <Card.Root class="flex flex-row justify-center items-center">
+    <Form.Root
+      form={form}
+      schema={ZQueryValues}
+      controlled
+      let:config
+    >
+      <Card.Content class="flex flex-row items-center gap-2 pt-6">
+        <Form.Field {config} name="origin">
+          <Form.Item>
+            <Form.Input placeholder="SFO"/>
+          </Form.Item>
+        </Form.Field>
+        <Form.Field {config} name="destination">
+          <Form.Item>
+            <Form.Input placeholder="ATH"/>
+          </Form.Item>
+        </Form.Field>
+        <Form.RangeCalendar />
+        <Form.Button>
+          {#await data.data}
+            <LoadingIcon class="mr-2 h-4 w-4 animate-spin" />
+          {:then}
+            <SearchIcon class="mr-2 h-4 w-4" />
+          {/await}
+          Search
+        </Form.Button>
+      </Card.Content>
+    </Form.Root>
+  </Card.Root>
+  {#await data.data}
+    Loading
+  {:then response}
+    {#if response}
+      <DataTable
+        summary={response.summary}
+        results={response.results}
+      />
+    {/if}
+  {/await}
 </div>
+
+
+<!-- {#if $page.form?.error}
+<Alert.Root variant="destructive">
+  <Alert.Title>
+    {`Error: ${$page.form.error.message}`}
+  </Alert.Title>
+  <Alert.Description>
+    {$page.form.error.helperText}
+  </Alert.Description>
+</Alert.Root>
+{/if} -->
