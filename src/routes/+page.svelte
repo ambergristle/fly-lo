21f93396cd38730e41cd8633873183250ef6b06a
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-02-15 07:01:51 -0800

    chore: cleanup page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import { superForm } from 'sveltekit-superforms/client';
-  import {
-    Loader2 as LoadingIcon, 
-    Search as SearchIcon, 
-  } from 'lucide-svelte';
+  
 
-  import type { PageData } from './$types';
+
+import type { PageData } from './$types';
 
   import * as Card from '$lib/components/card';
   import * as Form from '$lib/components/form';
@@ -18,6 +16,17 @@
     dataType: 'json',
   });
 </script>
+
+<!-- {#if $page.form?.error}
+<Alert.Root variant="destructive">
+  <Alert.Title>
+    {`Error: ${$page.form.error.message}`}
+  </Alert.Title>
+  <Alert.Description>
+    {$page.form.error.helperText}
+  </Alert.Description>
+</Alert.Root>
+{/if} -->
 
 <div class="container">
   <Card.Root class="flex flex-row justify-center items-center">
@@ -39,14 +48,6 @@
           </Form.Item>
         </Form.Field>
         <Form.RangeCalendar />
-        <Form.Button>
-          {#await data.data}
-            <LoadingIcon class="mr-2 h-4 w-4 animate-spin" />
-          {:then}
-            <SearchIcon class="mr-2 h-4 w-4" />
-          {/await}
-          Search
-        </Form.Button>
       </Card.Content>
     </Form.Root>
   </Card.Root>
@@ -59,15 +60,3 @@
     {/if}
   {/await}
 </div>
-
-
-<!-- {#if $page.form?.error}
-<Alert.Root variant="destructive">
-  <Alert.Title>
-    {`Error: ${$page.form.error.message}`}
-  </Alert.Title>
-  <Alert.Description>
-    {$page.form.error.helperText}
-  </Alert.Description>
-</Alert.Root>
-{/if} -->
